46239819a2e32f81ca145f01ba24f5452bb185a6
reotsugua <renanaugusto.ras@gmail.com>
2025-03-23 22:36:30 -0300

    navbar finalizada

--- primeiro-projeto/src/App.jsx
+++ primeiro-projeto/src/App.jsx
@@ -1,23 +1,11 @@
 import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./styles/App.css";
 
 class App extends React.Component {
   render() {
     return (
-      <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img src="" alt="" />
-            <h1>Space Flight News</h1>
-          </div>
-
-          <ul className="nav-list">
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Trending</a></li>
-            <li><a href="/">Categories</a></li>
-            <li><a href="/">About us</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Navbar />
     );
   }
 }
---
+++ primeiro-projeto/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import logoSvg from "../../assets/images/logo.svg";
+import './styles.css'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src={logoSvg} alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+
+          <ul className="nav-list">
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/">Trending</a>
+            </li>
+            <li>
+              <a href="/">Categories</a>
+            </li>
+            <li>
+              <a href="/">About us</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
+}
+
+export default Navbar;
