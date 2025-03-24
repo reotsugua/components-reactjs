e608b3b95107b308b49d19ad4aa901542afecb2a
reotsugua <renanaugusto.ras@gmail.com>
2025-03-23 21:35:24 -0300

    pt-br

--- primeiro-projeto/src/App.jsx
+++ primeiro-projeto/src/App.jsx
@@ -3,7 +3,21 @@ import React from "react";
 class App extends React.Component {
   render() {
     return (
-      <header></header>
+      <header>
+        <nav id="navbar">
+          <div className="nav-brand">
+            <img src="" alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+
+          <ul className="nav-list">
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Trending</a></li>
+            <li><a href="/">Categories</a></li>
+            <li><a href="/">About us</a></li>
+          </ul>
+        </nav>
+      </header>
     );
   }
 }
